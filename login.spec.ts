a232ae210148a951847659123760ef9e5a3b8305
Ashish Ghosh <Ashish.Ghosh2@ing.com>
2021-01-21 01:23:00 +0100

    Update login.spec.ts

--- login.spec.ts
+++ login.spec.ts
@@ -6,12 +6,12 @@ const accountData = require('./data/account.json')
 describe('Create User Functionality works', async()=>{
   it("Invalid Login",async({page})=>{
     const loginPage = new LoginPage(page)
-    console.log(baseData.url)
-    console.log(accountData.email)
     await loginPage.navigate(baseData.url)
+    await page.screenshot({ path: 'report/page1.png', fullPage: true });
     await loginPage.login(accountData.negativeLogin.username,accountData.negativeLogin.password)
+    await page.screenshot({ path: 'report/page2.png', fullPage: true });
     await loginPage.loginUnsuccessful()
-    
+    await page.screenshot({ path: 'report/page3.png', fullPage: true });
   })
   it.skip("login",async({page})=>{
     
