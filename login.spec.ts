9d00dd3d7195a4dba4ba6ce80f067773301e8765
Ashish Ghosh <Ashish.Ghosh2@ing.com>
2021-01-21 01:17:34 +0100

    Parameters changed 01

--- login.spec.ts
+++ login.spec.ts
@@ -1,16 +1,15 @@
 import {describe,it} from "./folio/testrunner.spec"
 import {LoginPage} from "./pages/login"
 const baseData = require('./data/base.json')
-const accountData = require('./data/base.json')
-const paymentData = require('./data/base.json')
-const productData = require('./data/base.json')
+const accountData = require('./data/account.json')
 
 describe('Create User Functionality works', async()=>{
   it("Invalid Login",async({page})=>{
     const loginPage = new LoginPage(page)
-
+    console.log(baseData.url)
+    console.log(accountData.email)
     await loginPage.navigate(baseData.url)
-    await loginPage.login(accountData.email,accountData.password)
+    await loginPage.login(accountData.negativeLogin.username,accountData.negativeLogin.password)
     await loginPage.loginUnsuccessful()
     
   })
