212b4d5ff64308ca34fcf2d3ab5267f8a1275fc7
Ashish Ghosh <Ashish.Ghosh2@ing.com>
2021-01-21 01:54:08 +0100

    Update login.spec.ts

--- login.spec.ts
+++ login.spec.ts
@@ -4,16 +4,20 @@ const baseData = require('./data/base.json')
 const accountData = require('./data/account.json')
 
 describe('Create User Functionality works', async()=>{
-  it("Invalid Login",async({page})=>{
+  it("Should validate Invalid Login",async({page})=>{
     const loginPage = new LoginPage(page)
     await loginPage.navigate(baseData.url)
-    await page.screenshot({ path: 'report/page1.png', fullPage: true });
     await loginPage.login(accountData.negativeLogin.username,accountData.negativeLogin.password)
-    await page.screenshot({ path: 'report/page2.png', fullPage: true });
     await loginPage.loginUnsuccessful()
-    await page.screenshot({ path: 'report/page3.png', fullPage: true });
   })
-  it.skip("login",async({page})=>{
-    
+  it("Should validate User Creation",async({page})=>{
+    const loginPage = new LoginPage(page)
+    let random = Math.floor(Math.random()*90000) + 10000;
+    let email = accountData.create.email.replace('Ashish','Ashish'+random); 
+    await loginPage.navigate(baseData.url)
+    accountData.create.email
+    await loginPage.createUser(accountData.create.firstname,accountData.create.lastname,email,accountData.create.password)
+    await loginPage.createUserSuccessful()
+    await page.screenshot({ path: 'report/page.png', fullPage: true });
   })
 })
